4d4b5a617a4155427c017a3270a4b05ee73e276c
Ling-Hao Lin <60977019+Mephisto-000@users.noreply.github.com>
2020-12-03 13:24:02 +0800

    Add Chapter4

---
+++ topic_2/secant.py
@@ -0,0 +1,15 @@
+#https://www.math.ubc.ca/~pwalls/math-python/roots-optimization/secant/
+
+
+def secant_method(f, x0, x1, tor, N, fl):
+
+    err = 1
+    itr0 = 1
+    while(err > tor and itr0 < N):
+        x = x1 -
+
+
+
+
+
+if __name__ == "__main__" :
